f646600d88d630bbc0f529acd75c08ae16143d28
Stanislav <staskur@gmail.com>
2020-07-07 12:55:01 +0400

    цветок с вводом количества лепестков

--- labs/2/lab2-10.py
+++ labs/2/lab2-10.py
@@ -1,30 +1,15 @@
-# окружность
+# цветок
 import turtle
-from math import sin,pi
 window = turtle.Screen()
-window.title(u"Окружность")
+window.title(u"цветок")
 turtle.shape('turtle')
 
-#r=30  # радиус
-#n=5   # количество секторов на длину окружности
+n = int(turtle.textinput(u"Развесистость цветка", "Введите количество лепестков: "))
+def figura(x):
+    for x in range(1,n+1):
+        turtle.circle(50)
+        turtle.left(360 / n)
 
-def figura(radius,num):    # радиус и количество секторов на длину окружности
-    dlina=abs(radius*2*sin(pi/num))   # длина стороны сектора положительное число
-    turtle.penup()
-    turtle.forward(radius)
-    turtle.left(90-180/num)   # начальный угол поворота головы
-    turtle.pendown()
-    for i in range(num):
-        turtle.left(360/num)
-        turtle.forward(dlina)
-    turtle.penup()
-    turtle.home()
-    return dlina
+figura (n)
 
-if __name__ == '__main__':
-    r = 50   # начальный радиус
-    for n in range(3, 13):
-        figura(r,n)
-        r+=25    # шаг увеличеняи радиуса
-
-    window.exitonclick()  # ждем щелчка мышки убрать картинку
+turtle.exitonclick()
