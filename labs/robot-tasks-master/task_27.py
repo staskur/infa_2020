a3064f67a573fac9f2a7ab9939e9695ba37c7565
Stanislav <staskur@gmail.com>
2020-07-15 14:11:44 +0400

    зарисовка с увеличивающимся шагом

--- labs/robot-tasks-master/task_27.py
+++ labs/robot-tasks-master/task_27.py
@@ -5,8 +5,14 @@ from pyrob.api import *
 
 @task
 def task_7_5():
-    pass
-
+    move_right()
+    n=1
+    while not wall_is_on_the_right():
+        fill_cell()
+        for i in range(n):
+            if not wall_is_on_the_right():
+                move_right()
+        n+=1
 
 if __name__ == '__main__':
     run_tasks()
