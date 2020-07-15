c90fad1220e6d9515b64c549326a0b4d22b72fd5
Stanislav <staskur@gmail.com>
2020-07-15 14:58:20 +0400

    остановки на клетках закрашенных

--- labs/robot-tasks-master/task_28.py
+++ labs/robot-tasks-master/task_28.py
@@ -5,7 +5,14 @@ from pyrob.api import *
 
 @task
 def task_7_6():
-    pass
+    n=0
+    while not wall_is_on_the_right():
+        if cell_is_filled():
+            n+=1
+        if n<5:
+            move_right()
+        else:
+            break
 
 
 if __name__ == '__main__':
