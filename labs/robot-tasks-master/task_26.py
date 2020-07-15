d7adfec650b55f873d4683101aaf92f20050a685
Stanislav <staskur@gmail.com>
2020-07-15 13:47:03 +0400

    зарисовка много много крестиков

--- labs/robot-tasks-master/task_26.py
+++ labs/robot-tasks-master/task_26.py
@@ -5,7 +5,35 @@ from pyrob.api import *
 
 @task(delay=0.02)
 def task_2_4():
-    pass
+    def krest():
+        move_down()
+        fill_cell()
+        move_right()
+        fill_cell()
+        move_right()
+        fill_cell()
+        move_left()
+        move_down()
+        fill_cell()
+        move_up(2)
+        fill_cell()
+        move_left()
+
+    for s in range(4):
+        for n in range(9):
+            krest()
+            move_right(4)
+        else:
+            krest()
+            move_left(36)
+        move_down(4)
+    else:
+        for n in range(9):
+            krest()
+            move_right(4)
+        else:
+            krest()
+            move_left(36)
 
 
 if __name__ == '__main__':
