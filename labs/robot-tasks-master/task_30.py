44493898a9b1ecdabb29635fb00f0f2c4e4a8f64
Stanislav <staskur@gmail.com>
2020-07-15 18:28:54 +0400

    четыре треугольника

--- labs/robot-tasks-master/task_30.py
+++ labs/robot-tasks-master/task_30.py
@@ -5,7 +5,21 @@ from pyrob.api import *
 
 @task(delay=0.01)
 def task_9_3():
-    pass
+    n = 0
+    while not wall_is_on_the_right():
+       n += 1
+       move_right()
+    move_left(n)
+
+    for i in range(n+1):
+        for j in range(n+1):
+            if i!=j and i!=n-j:
+                fill_cell()
+            if j!=n:
+                move_right()
+        move_left(n)
+        if i!=n:
+            move_down()
 
 
 if __name__ == '__main__':
