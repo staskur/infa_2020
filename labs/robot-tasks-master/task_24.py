2e5771d3130e9ddf21f8b2e1bf12ab71a55218aa
Stanislav <staskur@gmail.com>
2020-07-15 13:37:26 +0400

    зарисовка крестиков

--- labs/robot-tasks-master/task_24.py
+++ labs/robot-tasks-master/task_24.py
@@ -5,7 +5,24 @@ from pyrob.api import *
 
 @task
 def task_2_1():
-    pass
+    def krest():
+        move_down()
+        fill_cell()
+        move_right()
+        fill_cell()
+        move_right()
+        fill_cell()
+        move_left()
+        move_down()
+        fill_cell()
+        move_up(2)
+        fill_cell()
+        move_left()
+
+    move_down()
+    move_right()
+    krest()
+
 
 
 if __name__ == '__main__':
--- labs/robot-tasks-master/task_25.py
+++ labs/robot-tasks-master/task_25.py
@@ -5,7 +5,27 @@ from pyrob.api import *
 
 @task
 def task_2_2():
-    pass
+    def krest():
+        move_down()
+        fill_cell()
+        move_right()
+        fill_cell()
+        move_right()
+        fill_cell()
+        move_left()
+        move_down()
+        fill_cell()
+        move_up(2)
+        fill_cell()
+        move_left()
+
+    move_down()
+    for n in range(4):
+        krest()
+        move_right(4)
+    else:
+        krest()
+
 
 
 if __name__ == '__main__':
