66ca828d4ba6bae9999b5ff4e16413ddc6f22c10
Stanislav <staskur@gmail.com>
2020-07-15 12:12:13 +0400

    зарисовка треугольника

--- labs/robot-tasks-master/task_21.py
+++ labs/robot-tasks-master/task_21.py
@@ -5,8 +5,19 @@ from pyrob.api import *
 
 @task(delay=0.05)
 def task_4_11():
-    pass
+    n=1
+    for j in range(13):
 
+        move_down()
+        move_right()
+        for i in range(n):
+            fill_cell()
+            move_right()
+        for i in range(n+1):
+            move_left()
+        n+=1
+    move_down()
+    move_right()
 
 if __name__ == '__main__':
     run_tasks()
