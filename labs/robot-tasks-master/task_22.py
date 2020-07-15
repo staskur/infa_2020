5ad46c6e1e00198588fcfe48bcf61e9a0b15b742
Stanislav <staskur@gmail.com>
2020-07-15 12:51:28 +0400

    зарисовка произвольного прямоугольника

--- labs/robot-tasks-master/task_22.py
+++ labs/robot-tasks-master/task_22.py
@@ -5,7 +5,22 @@ from pyrob.api import *
 
 @task
 def task_5_10():
-    pass
+    def linia():
+        n=0
+        while not wall_is_on_the_right():
+            fill_cell()
+            move_right()
+            n+=1
+        else:
+            fill_cell()
+        if n!=0:
+            move_left(n)
+
+    while not wall_is_beneath():
+        linia()
+        move_down()
+    else:
+        linia()
 
 
 if __name__ == '__main__':
