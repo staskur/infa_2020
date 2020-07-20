80e6be8fc40346df6f985a91611eebf8260ed4cc
Stanislav <staskur@gmail.com>
2020-07-20 22:19:39 +0400

    добавлен второй способ подсчета

--- labs/proba/fib.py
+++ labs/proba/fib.py
@@ -4,6 +4,16 @@ def fib(n):
         return n
     return(fib(n-1)+fib(n-2))
 
+def fib2(n):
+    fi2 = [0, 1] + [0] * (n - 1)
+    for z in range(2, n + 1):
+        fi2[z] = fi2[z - 1] + fi2[z - 2]
+    return fi2[n]
 
-for k in range(16):
-    print(fib(k))
+m=30
+for k in range(m):
+    print(fib(k), end=" ")
+print()
+for k in range(m):
+    print(fib2(k), end=" ")
+print()
