ed2a45bcdd95ad57951356a578a103abd84d9050
Stanislav <staskur@gmail.com>
2020-07-20 16:46:41 +0400

    ползем вниз

--- labs/robot-tasks-master/task_31.py
+++ labs/robot-tasks-master/task_31.py
@@ -5,7 +5,16 @@ from pyrob.api import *
 
 @task(delay=0.01)
 def task_8_30():
-    pass
+    dyr=1
+    while dyr==1:
+        dyr=0
+        while wall_is_beneath() and not wall_is_on_the_left():
+            move_left()
+        if not wall_is_beneath():
+            move_down()
+            dyr=1
+            while not wall_is_on_the_right():
+                move_right()
 
 
 if __name__ == '__main__':
