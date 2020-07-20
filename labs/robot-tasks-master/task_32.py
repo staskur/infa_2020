c40ecb18809ca3d95ed50a41cd24a41e918b857a
Stanislav <staskur@gmail.com>
2020-07-20 19:07:37 +0400

    ползем вниз

--- labs/robot-tasks-master/task_32.py
+++ labs/robot-tasks-master/task_32.py
@@ -5,7 +5,26 @@ from pyrob.api import *
 
 @task(delay=0.01)
 def task_8_18():
-    pass
+        def stolb():
+            i=0
+            while not wall_is_above():
+                move_up()
+                if not cell_is_filled():
+                    fill_cell()
+                else:
+                    i+=1
+            while not wall_is_beneath():
+                move_down()
+            return(i)
+
+        n=0
+        while not wall_is_on_the_right():
+            if wall_is_above() and wall_is_beneath():
+                fill_cell()
+            if not wall_is_above():
+                n+=stolb()
+            move_right()
+        mov("ax",n)
 
 
 if __name__ == '__main__':
