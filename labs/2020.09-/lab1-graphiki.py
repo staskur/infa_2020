3bb8eb574adb275452107c3253237dbc703bf74d
Stanislav <staskur@gmail.com>
2020-10-30 10:13:07 +0400

    упражение 2 построение графиков окно с размерами и именем, легенда графиков

--- labs/2020.09-/lab1-graphiki.py
+++ labs/2020.09-/lab1-graphiki.py
@@ -5,17 +5,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 # задаем диапазон точек графика по абсциссе
 x = np.arange(-10, 10.01, 0.01)
+# задаем размер окна графика
+plt.figure('окно с графиками' , figsize=(10, 5))
 # рисуем виртуально график и добавляем на него элементы
-plt.plot(x, np.log(abs(x))/10)
-plt.plot(x,np.sin(x))
-plt.plot(x,np.cos(x))
+plt.plot(x, -x,label=r'$f_3(x)=-x$')
+plt.plot(x,np.sin(x),label=r'$f_1(x)=\sin(x)$')
+plt.plot(x,np.cos(x),label=r'$f_2(x)=\cos(x)$')
 # выводим сетку
 plt.grid('Yes')
 # надписи осей
 plt.xlabel(r'$x$')
 plt.ylabel(r'$f(x)$')
 # заголовок графика
-plt.title(r'$f_1(x)=\sin(x),\ f_2(x)=\cos(x),\ f_3(x)=\log(abs(x))/10$')
+plt.title(r'графики функций')
+plt.legend(loc='best', fontsize=12)
 
 # показываем получившийся график
 plt.show()
