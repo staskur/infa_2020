17454651e196abfc3fcd0151a1add868fc03baff
Stanislav <staskur@gmail.com>
2020-12-15 20:54:14 +0400

    добавлен файл перевода десятичного числа в другую систему счисления

--- labs/2020.09-/lab1-graphiki.py
+++ labs/2020.09-/lab1-graphiki.py
@@ -6,9 +6,10 @@ import matplotlib.pyplot as plt
 # задаем диапазон точек графика по абсциссе
 x = np.arange(-10, 10.01, 0.01)
 # задаем размер окна графика
-plt.figure('окно с графиками' , figsize=(10, 5))
+plt.figure('окно с двумя областями графиков' , figsize=(12, 5))
+# разбиваем на области для графиков
+sp = plt.subplot(1,2,1)
 # рисуем виртуально график и добавляем на него элементы
-plt.plot(x, -x,label=r'$f_3(x)=-x$')
 plt.plot(x,np.sin(x),label=r'$f_1(x)=\sin(x)$')
 plt.plot(x,np.cos(x),label=r'$f_2(x)=\cos(x)$')
 # выводим сетку
@@ -20,5 +21,15 @@ plt.ylabel(r'$f(x)$')
 plt.title(r'графики функций')
 plt.legend(loc='best', fontsize=12)
 
-# показываем получившийся график
+# выделяем вторую область
+sp = plt.subplot(1,2,2)
+plt.plot(x, -x,label=r'$f_3(x)=-x$')
+# заголовок графика
+plt.title(r'график одной функции')
+plt.legend(loc='best', fontsize=10)
+# переносим левую и нижнюю границы графика с осями и цифрами в центр, то есть делаем центральные оси
+sp.spines['left'].set_position('center')
+sp.spines['bottom'].set_position('center')
+
+# показываем получившееся окно с областями и графиками
 plt.show()
